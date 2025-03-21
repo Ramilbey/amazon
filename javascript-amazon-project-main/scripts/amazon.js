8c4c7f88eb5579a6c0e800f29fa8c151a33e0b12
Ramilbey <ramilbey505711503@gmail.com>
2025-03-21 16:15:41 +0800

    make add button interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,7 +1,6 @@
-  
-  let productsHTML = '';
-  products.forEach((product) => {
-      productsHTML +=   ` <div class="product-container">
+let productsHTML = "";
+products.forEach((product) => {
+  productsHTML += ` <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
           src="${product.image}">
@@ -20,7 +19,7 @@
       </div>
   
       <div class="product-price">
-        ${(product.priceCents/100).toFixed(2)}
+        ${(product.priceCents / 100).toFixed(2)}
       </div>
   
       <div class="product-quantity-container">
@@ -45,10 +44,33 @@
         Added
       </div>
   
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+        product.id
+      }">
         Add to Cart
       </button>
-    </div>`
-      
-  })
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+    </div>`;
+});
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId= button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId  === item.productId) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId : productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart)
+  });
+});
